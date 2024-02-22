391ded34deb847ebfc389280dafbb4706d6d0337
baggary <ibraheema.a2014@gmail.com>
2024-02-22 18:19:14 +0300

    update router for glowingspotlight component

--- src/Router/Routes.Router.tsx
+++ src/Router/Routes.Router.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import TabControl from "../Components/TabControl.component";
+import TabControl from "../Components/TabControl/TabControl.component";
+import GlowingSpotlight from "../Components/GlowingSpotLight/GlowingSpotlight.component";
 
 export default function Routess() {
   return (
-    <Routes>
-      <Route path="/tabcontrol" element={<TabControl />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/tabcontrol" element={<TabControl />} />
+        <Route path="/glowingspotlight" element={<GlowingSpotlight />} />
+      </Routes>
+    </div>
   );
 }
